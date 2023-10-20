3e03efa70609821125daecb0c13f5c01163d9b39
csaben <clark@saben.org>
2023-10-20 11:22:08 -0400

    folder infra v1

---
+++ medicalLM/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MedicalLM',
+    packages=find_packages(),
+    version='0.1.0',
+    description='medical language model for diagnosis based on natural language',
+    author=['Clark Saben', 'Andrew Ceniccola'],
+    license='MIT'
+)
