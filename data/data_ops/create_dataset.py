06d95938e4dcb22aa6161bf4b6fe7dba6a139f93
Andrew-Ceniccola <andrewsamce@me.com>
2023-10-23 18:12:36 -0400

    basic data manipulation

--- data/data_ops/create_dataset.py
+++ data/data_ops/create_dataset.py
@@ -1,10 +1,17 @@
-from datasets import list_datasets, load_dataset
+from datasets import load_dataset
 from pprint import pprint
+import pandas as pd
 
 
-medqa_dataset = load_dataset("bigbio/med_qa")  
-medmcqa_dataset = load_dataset("medmcqa")
-pubmedqa_dataset = load_dataset("pubmed_qa", "pqa_artificial") 
-liveqa_dataset = load_dataset("liveqa")
-medicationqa_dataset = load_dataset("truehealth/medicationqa")
-medicationqa_dataset = load_dataset("cais/mmlu", "all")
+datasets = {}
+
+#datasets['medqa'] = load_dataset("bigbio/med_qa") # multiple choice with minimal context
+#datasets['medmcqa'] = load_dataset("medmcqa") # multiple choice questions with explaination
+datasets['pubmedqa'] = load_dataset("pubmed_qa", "pqa_artificial") # yes or no with long anwser (and context)
+#datasets['liveqa'] = load_dataset("liveqa") # 'passages' only 
+#datasets['medicationqa'] = load_dataset("truehealth/medicationqa") # short response given question
+#datasets['mmlu'] = load_dataset("cais/mmlu", "all") # selection of multiplechoice questions
+
+
+datasets['pubmedqa']['train'].set_format("pandas")
+print(len(datasets['pubmedqa']['train']))
