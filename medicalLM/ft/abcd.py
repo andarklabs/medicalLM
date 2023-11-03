a966669caa04cca5466043d882d5420cb8a288d8
csaben <clark@saben.org>
2023-11-03 10:47:09 -0400

    progress on ft, not perf training logic, inference code breaking

---
+++ medicalLM/ft/abcd.py
@@ -0,0 +1,8 @@
+"""ft using the following two resources:
+
+blog post:
+https://erichartford.com/uncensored-models
+
+adjusted locations:
+https://github.com/nlpxucan/abcd/tree/main/WizardLM#fine-tuning
+"""
